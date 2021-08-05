17fa68e4db89e3c2cbeb19bfc3ae6eaad6ebb9e1
PelumiWeb <pelumiogundipe905@gmail.com>
2021-08-05 12:40:18 +0100

    first commit

---
+++ screens/MapSreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const MapSreen = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default MapSreen
+
